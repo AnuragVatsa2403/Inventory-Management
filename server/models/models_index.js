9900b14599c5da91c56224d2d4c236f39546229f
Anurag <avatsa55@gmail.com>
2026-03-27 11:31:01 +0530

    +47

---
+++ server/models/models_index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  User:            require('./User'),
+  Product:         require('./Product'),
+  Category:        require('./Category'),
+  Supplier:        require('./Supplier'),
+  PurchaseOrder:   require('./PurchaseOrder'),
+  GoodsReceipt:    require('./GoodsReceipt'),
+  StockLedger:     require('./StockLedger'),
+  SalesOrder:      require('./SalesOrder'),
+  ProductionEntry: require('./ProductionEntry'),
+};
